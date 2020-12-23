77ae3dfcca87e36adfca0450db76c2c1f3b60ed2
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-23 16:55:15 +0100

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,20 @@
 
 using namespace Cool;
 
-
 int main() {
 	// Init
 	Log::Initialize();
 	RenderState::SubscribeToSizeChanges([]() {Log::Info("The size is now {} {}", RenderState::Size().width(), RenderState::Size().height()); });
 	// Create image
 	Image image(200, 200);
+	// Register the rendering size
 	RenderState::setExportSize(image.width(), image.height());
 	RenderState::setIsExporting(true);
 	// Drawing
 	Camera camera;
 	for (Pixel px : image) {
 		Ray ray = camera.rayThroughPixel({ px.xInt(), px.yInt() });
-		if (ray.direction().y > 0)
+		if (ray.direction().z > 0)
 			px.color().set(0.8f, 0.1f, 0.4f);
 		else
 			px.color().set(0.95f, 0.7f, 0.8f);
