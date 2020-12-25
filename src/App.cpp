3ebc6f22f3419b88dca6d83a62dd950b7f0bb38f
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-25 14:29:25 +0100

    Successfully grab the intersection ray-sphere

--- src/App.cpp
+++ src/App.cpp
@@ -1,24 +1,33 @@
 #include "App.h"
 
 #include <Cool/App/RenderState.h>
-#include <c3ga/c3gaTools.hpp>
 
 App::App()
-	: m_image(200, 200), m_displayTextureShader("Cool/Renderer_Fullscreen/fullscreen.vert", "shaders/displayTexture.frag")
+	: m_image(100, 100), m_displayTextureShader("Cool/Renderer_Fullscreen/fullscreen.vert", "shaders/displayTexture.frag")
 {
 	// Register the rendering size
 	RenderState::setExportSize(m_image.width(), m_image.height());
 	RenderState::setIsExporting(true);
 	// Drawing
+	auto dualSphere = c3ga::dualSphere<double>(0., 0., 0., 1);
 	for (Pixel px : m_image) {
+		// Get ray
 		Ray ray = m_camera.rayThroughPixel({ px.xInt(), px.yInt() });
-		if (ray.direction().z > 0)
-			px.color().set(0.8f, 0.1f, 0.4f);
-		else
-			px.color().set(0.95f, 0.7f, 0.8f);
+		auto dir = (
+			ray.direction().x * c3ga::e1<double>() +
+			ray.direction().y * c3ga::e2<double>() +
+			ray.direction().z * c3ga::e3<double>()
+		);
+		auto pos = c3ga::point<double>(ray.origin().x, ray.origin().y, ray.origin().z);
+		auto rayLine = pos ^ dir ^ c3ga::ei<double>();
+		// Intersection
+		auto intersectionPtPair = !(dualSphere ^ (!rayLine));
+		// Coloring
+		double square = intersectionPtPair | intersectionPtPair;
+		px.color().set(square * 0.4 + 0.5, 0.2f, square > 0 ? 1.f : 0.f);
 	}
 	// Put in the texture
-	m_texture.genTexture();
+	m_texture.genTexture(GL_NEAREST);
 	m_texture.uploadRGBA(m_image.width(), m_image.height(), m_image.data());
 	// Save
 	//m_image.saveAs("out/test.png");
